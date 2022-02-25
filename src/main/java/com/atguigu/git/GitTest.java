ae4468d4d1dda797d502adb30d11926da6bb770b
shamozhihu <18595971961@163.com>
2022-02-25 21:48:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+* @author shamozhihu0214
+*
+**/
+
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git");
+    }
+}
